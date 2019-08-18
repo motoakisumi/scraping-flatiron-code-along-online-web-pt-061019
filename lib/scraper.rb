04fec6ad90127300511f581980eddf47b8bcd910
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-08-18 06:12:05 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,6 +1,5 @@
 require 'nokogiri'
 require 'open-uri'
-require 'pry'
 
 require_relative './course.rb'
 
@@ -22,4 +21,18 @@ class Scraper
       course.description = post.css("p").text
     end
   end
+
+  def print_courses
+    self.make_courses
+    Course.all.each do |course|
+      if course.title && course.title != ""
+        puts "Title: #{course.title}"
+        puts "  Schedule: #{course.schedule}"
+        puts "  Description: #{course.description}"
+      end
+    end
+  end
+
 end
+
+Scraper.new.print_courses
