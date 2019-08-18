89b50283119a6b68051328e0aee46fbed36bdb56
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-08-18 05:29:10 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,17 +7,9 @@ class Scraper
 
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-  end
 
-    def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title && course.title != ""
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
-      end
-    end
+    binding.pry
   end
-
 end
+
+Scraper.new.get_page
