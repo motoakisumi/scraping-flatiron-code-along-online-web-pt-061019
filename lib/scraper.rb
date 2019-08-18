2f0aafc6b6a4e2a691bf9427cb8f47917e061ff8
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-08-18 06:06:03 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,13 +6,15 @@ require_relative './course.rb'
 
 class Scraper
 
-  doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
 
-  #doc.css(".post").each do |post|
-    #course = Course.new
-    #course.title = post.css("h2").text
-    #course.schedule = post.css(".date").text
-    #course.description = post.css("p").text
+  def get_page
+
+    Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+      #doc.css(".post").each do |post|
+        #course = Course.new
+        #course.title = post.css("h2").text
+        #course.schedule = post.css(".date").text
+        #course.description = post.css("p").text
   end
   binding.pry
 end
